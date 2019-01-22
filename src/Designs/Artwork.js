42726dfc097e43ad44e7541a8fdbd5232c7e7acb
Jesse <jsainz237@gmail.com>
2019-01-22 17:06:52 -0600

    new sections

---
+++ src/Designs/Artwork.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../app.css'
+
+
+const Artwork = () => (
+    <div className='section-container' style={{ height: 300, width: '100vw', backgroundColor: '#aedae2'}}>
+        <p>hello</p>
+    </div>
+)
+
+export default Artwork
