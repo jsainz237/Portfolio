15a40e071ae33874bfbfa0d0411cf4a061747ec2
Jesse <jsainz237@gmail.com>
2019-01-23 12:12:34 -0600

    working on grid

---
+++ src/Projects/projects.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './projects.css'
+
+import arrow from '../assets/designs/arrow.png'
+
+const Projects = () => (
+    <div className='section-container' style={{ height: 500, width: '100vw', backgroundColor: '#85BDBF' }}>
+        <div className='grid-container'>
+            <div className="grid-item item1" />
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+        </div>
+    </div>
+)
+
+export default Projects;
