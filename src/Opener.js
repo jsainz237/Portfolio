7a3b67a5dd435ee3a632630391f707a675f74804
Jesse <jsainz237@gmail.com>
2019-02-12 23:18:43 -0600

    fixed logo overlap issue on mobile and resized some text and links

--- src/Opener.js
+++ src/Opener.js
@@ -4,6 +4,21 @@ import Tilt from 'react-tilt'
 
 import logoSqaure from './assets/images/logo-sqaure.png'
 import logoLetters from './assets/images/logo-letters.png'
+import fullLogo from './assets/images/fullLogo.png'
+import { isMobile } from 'react-device-detect';
+
+const tiltLogo = (
+    <Tilt options={{ max: 20, perspective: 1500, reverse: true }} style={{ transformStyle: 'preserve-3d' }}>
+        <div className="tilt-container" style={{ position: 'relative', transformStyle: 'preserve-3d' }}>
+            <img src={logoSqaure} alt="" id="logo-sqaure"/>
+            <img src={logoLetters} alt="" id="logo-letters" style={{ transform: 'translateZ(30px)'}}/>
+        </div>
+    </Tilt>
+)
+
+const regLogo = (
+    <img src={fullLogo} alt="" style={{ maxHeight: '210px', maxWidth: '250px', width: '40vw', height: '33vw'}}/>
+)
 
 var Home = () => (
     <div className='section-container' style={{ height: '100vh', width: '100vw', backgroundColor: '#171717'}}>
@@ -11,12 +26,7 @@ var Home = () => (
             
             {/* Logo render */}
             <div id='logo'>
-                <Tilt options={{ max: 20, perspective: 1500, reverse: true }} style={{ transformStyle: 'preserve-3d'}}>
-                    <div className="tilt-container" style={{ position: 'relative', transformStyle: 'preserve-3d' }}>
-                        <img src={logoSqaure} alt="logo-square" id="logo-sqaure"/>
-                        <img src={logoLetters} alt="logo-letters" id="logo-letters" style={{ transform: 'translateZ(30px)'}}/>
-                    </div>
-                </Tilt>
+                { isMobile ? regLogo : tiltLogo }
             </div>
 
             <About />
