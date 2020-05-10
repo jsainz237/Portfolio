b8d0314a421952082868305fabd9a74fd009bb73
Jesse <jsainz237@gmail.com>
2020-05-10 17:41:23 -0500

    add mobile check to prevent blur on dev tile

--- src/DevProjects/Tile/Tile.index.js
+++ src/DevProjects/Tile/Tile.index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { isMobile } from 'react-device-detect';
 
 import './Tile.styles.css';
 
@@ -7,7 +8,7 @@ export function Tile({ image, title, description, link, linkTitle }) {
 
     return (
         <div className='tile-container'>
-            <img src={image} alt="HELLO" style={{ filter: showOverlay ? 'blur(10px)' : null }} className='tile-img' onMouseEnter={() => setShowOverlay(true)} />
+            <img src={image} alt="HELLO" style={{ filter: showOverlay && !isMobile ? 'blur(10px)' : null }} className='tile-img' onMouseEnter={() => setShowOverlay(true)} />
             <div className='tile-overlay' style={{ display: showOverlay ? 'flex' : 'none' }} onMouseLeave={() => setShowOverlay(false)}>
                 <p className='tile-title'>{title}</p>
                 <p className='tile-description'>{description}</p>
